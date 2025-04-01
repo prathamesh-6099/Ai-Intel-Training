8e2039204effd79e530f543681007f203f89bb8a
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2025-04-01 01:37:08 -0700

    Modified requirements.txt

--- api-tasks-models/translation/translation/client.py
+++ api-tasks-models/translation/translation/client.py
@@ -7,17 +7,18 @@ def main():
     hi_text = "जीवन एक चॉकलेट बॉक्स की तरह है।"
     chinese_text = "生活就像一盒巧克力。"
     start = time.time()
-
-    for i in range(100):
         
-        translated_hi_text = translator.translate(hi_text, "hi", "fr")
-        print(f"Hindi to French: {translated_hi_text}")
-
-    # Translate Chinese to English
+    translated_hi_text = translator.translate(hi_text, "hi", "fr")
+    print(f"Hindi to French: {translated_hi_text}")
     total = time.time() - start
+    print("Total Time taken: ", total)
+
+    start = time.time()
+    translated_zh_text = translator.translate(chinese_text, "zh", "en")
+    print(f"Chinese to English: {translated_zh_text}")
+    total = time.time() - start
+    print("Total Time Taken: ", total)
     print(total)
-    # translated_zh_text = translator.translate(chinese_text, "zh", "en")
-    # print(f"Chinese to English: {translated_zh_text}")
 
 if __name__ == "__main__":
     main()
